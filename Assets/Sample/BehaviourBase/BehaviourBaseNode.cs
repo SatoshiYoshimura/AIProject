ef68738b9574ed0120891334c82d2880d90e8644
unknown <>
2017-01-18 01:55:18 +0900

    クラス図簡易版ができた

---
+++ Assets/Sample/BehaviourBase/BehaviourBaseNode.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ai.BehaviourBase {
+    public abstract class BehaviourBaseNode {
+
+    }
+}
---
+++ Assets/Sample/BehaviourBase/NonActionNode.cs
@@ -0,0 +1,1 @@
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;namespace Ai.BehaviourBase {    public class NonActionNode : BehaviourBaseNode {    }}
