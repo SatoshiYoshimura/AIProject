64a6ed66221e3b5883c2fc2f33b11087f6f025e0
unknown <>
2017-01-18 02:04:46 +0900

    NonActionNode

--- Assets/Sample/BehaviourBase/NonActionNode.cs
+++ Assets/Sample/BehaviourBase/NonActionNode.cs
@@ -1,1 +1,3 @@
-﻿using System.Collections;using System.Collections.Generic;using UnityEngine;namespace Ai.BehaviourBase {    public class NonActionNode : BehaviourBaseNode {    }}
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;namespace Ai.BehaviourBase {    public abstract class NonActionNode : BehaviourBaseNode {        protected List<BehaviourBaseNode> nodeList = null;        private bool isRoot = false;
+
+        protected abstract void DoExecute();        protected abstract void DecideEffectiveNodes();    }}
